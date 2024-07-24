f5850643441a3972a114d8a37c9a51ed892827db
Tristan-GPT <tristangpt.com.gtb@gmail.com>
2024-07-24 18:38:12 +0200

    fix: fix some esthetic details

--- src/pages/Unique/Unique.tsx
+++ src/pages/Unique/Unique.tsx
@@ -5,7 +5,7 @@ function Unique() {
     return (
         <>
         <h1>I'm <span className='title'>Tristan-GPT</span><br /> & I'm a developer</h1>
-        <h1>
+        <h1 className='imgs'>
             <a href="mailto:tristangpt.pro@gmail.com"><img src="/mail.svg" alt="" className='images'/></a>
             <a href="https://www.discord.com/users/705425854825496656"><img src="/discord.png" alt="" className='images'/></a>
             <a href="https://github.com/Tristan-GPT"><img src="/github.png" alt="" className='images'/></a>
