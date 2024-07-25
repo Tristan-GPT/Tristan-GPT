499ec14ce8a4b633332c6a93599db0d283fe3eaf
Tristan-GPT <tristangpt.com.gtb@gmail.com>
2024-07-25 20:12:29 +0200

    fix: remove ESLint

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -3,7 +3,7 @@ import './Sponsors.css';
 import Navbar from '../../components/Navbar/Navbar';
 
 function Sponsors() {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
     const [menuOpen, setMenuOpen] = useState(false);
 
   const handleMenuToggle = (isOpen: boolean | ((prevState: boolean) => boolean)) => {
